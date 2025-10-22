4dcd84544a60edd451fccfce0579d99eda381f25
Laudya Aprilia <luludea95@gmail.com>
2025-10-22 17:41:22 +0800

    update on index

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
 
-// Redirect ke halaman login jika belum login
 if (!isset($_SESSION['user_id'])) {
     header("Location: auth/login.php");
     exit();
 }
 
-// Redirect berdasarkan role
 if ($_SESSION['role'] === 'admin') {
     header("Location: admin/dashboard.php");
 } else {
